04972b9cef981a555f6764a672afbf2cb6d5549d
surajnani-99 <azureuser@test-vm.oki2hykgddnu3mekzy2w43camf.rx.internal.cloudapp.net>
2026-01-14 10:22:45 +0000

    updated all files

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -1,28 +1,31 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Sample API",
-        Version = "v1"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Enable Swagger
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample API v1");
-    c.RoutePrefix = string.Empty;
+    c.RoutePrefix = string.Empty; // Swagger at root
 });
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
+
+
+
+
+
+
+
+
+
+
